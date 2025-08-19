bbe91e520131ca6682281aa28b90ade9d8e236c6
rahul pareek <itsrahul615@gmail.com>
2025-08-19 22:44:42 +0530

    added category

--- src/Fetch.jsx
+++ src/Fetch.jsx
@@ -5,28 +5,25 @@ import axios from 'axios'
 
 const Fetch = () => {
 
-  const [data, setData] = useState("")
+  const [data, setData] = useState([])
   const [loading, isLoading] = useState(true)
   const [search, setSearch] = useState("")
   const [sort, setSort] = useState("")
   // const navigate = useNavigate()
+  const [cart, setCart] = useState([])
+  const [category,setCategroy] = useState([])
+  const [selectedCategory, setSelectedCategory] = useState("")
 
   const fetchData = async () => {
     try {
       const result = await axios.get("https://fakestoreapi.com/products");
       setData(result.data)
       isLoading(false)
+      const unique = [...new Set(result.data.map(item => item.category))]
+      setCategroy(unique);
     } catch (e) {
       console.log("error>>>", e);
     }
-
-    const searchingandSorting = data
-    .filer(item=> 
-      item.title.toLowerCase().includes(search.toLowerCase()) ||
-      item.category.toLowerCase().includes(search.toLowerCase())
-    ).sort((a,b) =>{
-      if(sort == lowToHigh) return a.price - b.price;
-    })
     // fetch("https://dummyjson.com/products")
     //   // .then(res=>{console.log(">>res",res)})
     //   .then(res => res.json())
@@ -42,40 +39,94 @@ const Fetch = () => {
   //   }
   // }
 
+  const searchingandSorting = data
+    .filter(item =>
+      item.title.toLowerCase().includes(search.toLowerCase()) ||
+      item.category.toLowerCase().includes(search.toLowerCase())
+    ).filter(item=> 
+      selectedCategory ? item.category === selectedCategory : true
+    )
+    .sort((a, b) => {
+      if (sort == "lowToHigh") return a.price - b.price;
+      if (sort == "highToLow") return b.price - a.price;
+      if (sort == "ascen") return a.title.localeCompare(b.title)
+      if (sort == "desc") return b.title.localeCompare(a.title)
+      return 0;
+    })
+
+  const addToCart = (product) => {
+    const exists = cart.some(item => item.id === product.id)
+    if (!exists) {
+      setCart([...cart, product]);
+    }
+  }
+
+  const inCart = (productId) => {
+    return cart.some(item => item.id === productId);
+  }
+
+  const removeFromCart = (productId) => {
+    setCart(cart.filter(item => item.id !== productId))
+  }
+
   useEffect(() => {
     fetchData()
   }, []);
 
-  console.log(data)
+  console.log(">>>data ", data)
+  console.log(">>>cart ", cart)
+  console.log(">>>uniquecategory",category)
+  console.log(">>>selectedCatrogry",selectedCategory)
 
   return (
-    <div style={{margin: "80px 0px"}}>
+    <div style={{ margin: "80px 0px" }}>
       {/* <button onClick={toggleEvent}>Change</button> */}
-      <input style={{width: "60%",padding:"20px" , border:"none",margin:"0 30px 20px 0"}}
+      <input style={{ width: "60%", padding: "20px", border: "none", margin: "0 30px 20px 0" }}
         type="text"
         placeholder='Search any item or category'
         value={search}
-        onChange={(e)=> {setSearch(e.target.value)}}
+        onChange={(e) => { setSearch(e.target.value) }}
       />
 
-      <select style={{padding:"10px"}} value={sort} onChange={(e) =>{setSort(e.target.value)}}>
+      <select style={{ padding: "10px" }} value={sort} onChange={(e) => { setSort(e.target.value) }}>
         <option value="">Sort By</option>
         <option value="lowToHigh">Low to High</option>
         <option value="highToLow">High to Low</option>
         <option value="ascen">A-Z</option>
         <option value="desc">Z-A</option>
       </select>
+      
+      <select value={selectedCategory} onChange={(e)=>setSelectedCategory(e.target.value)}>
+        <option value="">Category</option>
+        {category.map(cat =>(
+          <option value={cat}>{cat}</option>
+        ))}
+      </select>
+
+      
+
+      <h3>Cart Items = {cart.length}</h3>
 
       {loading ? <p>data is loading...</p> :
 
         <div style={{ display: "flex", flexWrap: "wrap", gap: "20px" }}>
-          {data.map((item) => (
-            <div style={{ border: "2px solid black", padding: "20px", width: "410px", boxSizing: "border-box", }}>
+          {searchingandSorting.map((item) => (
+            <div style={{ border: "2px solid black", padding: "20px", width: "380px", boxSizing: "border-box", }}>
               <img src={item.image} style={{ height: "150px" }} />
               <p><b>Title: </b>{item.title}</p>
               <p><b>Description: </b>{item.description}</p>
               <p><b>Category: </b>{item.category}</p>
               <p>Price: ${item.price}</p>
+
+              {inCart(item.id) ? (
+                <button onClick={() => removeFromCart(item.id)}>
+                  Remove From Cart
+                </button>
+              ) : (
+                <button onClick={() => addToCart(item)}>
+                  Add To Cart
+                </button>
+              )}
             </div>
           ))}
         </div>
